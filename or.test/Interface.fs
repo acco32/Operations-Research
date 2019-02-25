a4c179149b5bd25a33be5d8e33d6d9c0672cf613
Matthew Moore <>
2019-02-24 23:46:47 -0800

    Proper expressions. still incomplete.

--- or.test/Interface.fs
+++ or.test/Interface.fs
@@ -7,32 +7,37 @@ module ``Basic Interface`` =
   open Operations.Research.Types
   open Operations.Research.LinearProgramming
 
-  [<Fact>]
-  let ``Create Boolean Variable with default values``() =
-    let varName = "bool"
-    let v = Variable.Boolean varName
-    v.LowerBound |> should equal 0.0
-    v.UpperBound |> should equal 1.0
-    v.State |> should equal false
-    v.Value |> should equal 0.0
-    v.Name |> should equal varName
+  // [<Fact>]
+  // let ``Create Boolean Variable with default values``() =
+  //   let varName = "bool"
+  //   let v = Variable.Boolean varName
+  //   v.LowerBound |> should equal 0.0
+  //   v.UpperBound |> should equal 1.0
+  //   v.State |> should equal false
+  //   v.Value |> should equal 0.0
+  //   v.Name |> should equal varName
 
-  [<Fact>]
-  let ``Create Number Variable with default values``() =
-    let varName = "real"
-    let lb = -1.0
-    let ub = 2.0
-    let v = Variable.Number varName lb ub
-    v.LowerBound |> should equal lb
-    v.UpperBound |> should equal ub
-    v.Value |> should equal 0.0
-    v.Name |> should equal varName
+  // [<Fact>]
+  // let ``Create Number Variable with default values``() =
+  //   let varName = "real"
+  //   let lb = -1.0
+  //   let ub = 2.0
+  //   let v = Variable.Number varName lb ub
+  //   v.LowerBound |> should equal lb
+  //   v.UpperBound |> should equal ub
+  //   v.Value |> should equal 0.0
+  //   v.Name |> should equal varName
+
+
 
   [<Fact>]
   let ``Create Boolean Operand`` () =
-    let x = Variable.Boolean ""
-    // let res =  2.0 * x
-    let result = Operand(2.0, x)
+    let x = Variable.Num "" 0.0 1.0
+    let y = Variable.Bool ""
+
+    let res_x = 1.0*x + 1.0
+    let res_y = 2.0*y + 1.0
+
     // result.Value |> should equal 0.0
     // x.True() = 1.0
 
@@ -44,6 +49,5 @@ module ``Basic Interface`` =
     // let expr = [] + 3*x + 1*z + -2*y + 6*x
     // let con1 = expr <== 6.0
     // let con2 = expr >== 6.0
-
 
 
--- or/types.fs
+++ or/types.fs
@@ -3,62 +3,100 @@ namespace Operations.Research
 module Types =
   open System
 
-  type VariableClass =
-    | Boolean
-    | Number
-
-  type Variable = {
-    Name: string;
-    Class: VariableClass;
-    LowerBound: float;
-    UpperBound: float;
-    Value: float;
-  }
-  with
-    static member Boolean name =
-      { Name=name; Class=Boolean; LowerBound=0.0; UpperBound=1.0; Value=0.0 }
-    static member Number name lowerBound upperBound =
-      { Name=name; Class=Number; LowerBound=lowerBound; UpperBound=upperBound; Value=0.0 }
-    member this.State =
-      match this.Class with
-      | Boolean -> this.Value > 0.5
-      | _ -> failwith "Cannot get state on number variable"
-
-
-  type Operand =
-    | Operand of float * Variable
-    | Variable of Variable
+  type Variable =
+    | Boolean of name:string * value:bool
+    | Number of name:string * lowerBound:float * upperBound:float * value:float
+    static member Bool (name:string) =
+      Boolean(name, false)
+    static member Num (name:string) (lowerBound:float) (upperBound:float) =
+      Number(name, lowerBound, upperBound, 0.0)
+    static member (*) (c:float, v:Variable) = Compound(c, v)
+    member this.Set (value) =
+      Boolean("", false)
+      // pattern match on value's type then ensure it is not out of bounds then set
+  and Operand =
+    | Compound of float * Variable
     | Value of float
-    | Add of Operand list
+    | Expression of Operand list
     static member (+) (o1:Operand, o2:Operand) =
-        match o1,o2 with
-        | Add(ops1), Add(ops2) -> Add(ops1 @ ops2)
-        | Add(ops1), ops2 -> Add(ops1 @ [ ops2 ])
-        | ops1, Add(ops2) -> Add(ops1 :: ops2)
-        | ops1, ops2 -> Add [ ops1 ; ops2 ]
-    static member (+) (op:Operand, num:float) = op + Value num
-    static member (+) (num:float, op:Operand) = Value num + op
-
-
-
-  type Operand1 = {
-    Coefficient: float
-    Variable: Variable
-    Value: float
-  }
-  with
-    // static member (*) (var:Variable, coeff:float) =
-    //   { Coefficient=coeff; Variable=var; Value=var.Value*coeff }
-    static member (|*|) (coeff:float, var:Variable) =
-      { Coefficient=coeff; Variable=var; Value=var.Value*coeff }
-    // static member (+) (op1:Operand, op2:Operand) =
-    //   {op1.Value}
+      match o1,o1 with
+      | Expression(a), Expression(b) -> Expression( a @ b )
+      | Expression(l), Value(v) -> Expression( l @ [Value(v)]  )
+      | Expression(l), Compound(c, v) -> Expression( l @ [Compound(c, v)] )
+      | Value(v), Expression(l) -> Expression( [Value(v)] @ l)
+      | Compound(c, v), Expression(l) -> Expression( [Compound(c, v)] @ l)
+    static member (+) (o:Operand, v:float) = o + Value(v)
+    static member (+) (v:float, o:Operand) = Value(v) + o
 
 
 
 
 
+  // let inline (+) (ops:Expression) (e:Operand) : Expression = List.append ops [e]
 
+  // type Constraint = Constraint of Expression * lowerBound:float * upperBound:float
+
+  // let inline (<==) (e:Expression) (ub:float)  = Constraint(e, Double.NegativeInfinity, ub)
+  // let inline (>==) (e:Expression) (lb:float)  = Constraint(e, lb, Double.NegativeInfinity)
+  // let inline (==) (e:Expression) (v:float)  = Constraint(e, v, v)
+
+  (**************************************************************************************)
+
+  // type VariableClass =
+  //   | Boolean
+  //   | Number
+
+  // type Variable = {
+  //   Name: string;
+  //   Class: VariableClass;
+  //   LowerBound: float;
+  //   UpperBound: float;
+  //   Value: float;
+  // }
+  // with
+  //   static member Boolean name =
+  //     { Name=name; Class=Boolean; LowerBound=0.0; UpperBound=1.0; Value=0.0 }
+  //   static member Number name lowerBound upperBound =
+  //     { Name=name; Class=Number; LowerBound=lowerBound; UpperBound=upperBound; Value=0.0 }
+  //   member this.State =
+  //     match this.Class with
+  //     | Boolean -> this.Value > 0.5
+  //     | _ -> failwith "Cannot get state on number variable"
+
+
+  // type Operand =
+  //   | Operand of float * Variable
+  //   | Variable of Variable
+  //   | Value of float
+  //   | Add of Operand list
+  //   static member (+) (o1:Operand, o2:Operand) =
+  //       match o1,o2 with
+  //       | Add(ops1), Add(ops2) -> Add(ops1 @ ops2)
+  //       | Add(ops1), ops2 -> Add(ops1 @ [ ops2 ])
+  //       | ops1, Add(ops2) -> Add(ops1 :: ops2)
+  //       | ops1, ops2 -> Add [ ops1 ; ops2 ]
+  //   static member (+) (op:Operand, num:float) = op + Value num
+  //   static member (+) (num:float, op:Operand) = Value num + op
+
+
+  (**************************************************************************************)
+
+
+  // type Operand1 = {
+  //   Coefficient: float
+  //   Variable: Variable
+  //   Value: float
+  // }
+  // with
+  //   // static member (*) (var:Variable, coeff:float) =
+  //   //   { Coefficient=coeff; Variable=var; Value=var.Value*coeff }
+  //   static member (|*|) (coeff:float, var:Variable) =
+  //     { Coefficient=coeff; Variable=var; Value=var.Value*coeff }
+  //   // static member (+) (op1:Operand, op2:Operand) =
+  //   //   {op1.Value}
+
+
+  (**************************************************************************************)
 
 
   // type Variable =
