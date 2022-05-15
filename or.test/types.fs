8b43c2132562cbefc73ba28d38a1ec33724f1af8
Matthew Moore <>
2022-05-14 17:22:53 -0700

    save work

--- or.test/types.fs
+++ or.test/types.fs
@@ -6,17 +6,17 @@ module ``Basic Types`` =
   open Xunit
   open FsUnit.Xunit
   open Operations.Research.Types
-  open Operations.Research.Models
+
 
   [<Fact>]
   let ``create boolean variable with default values``() =
     let varName = "bool"
-    let v = Variable.Bool varName
+    let v = Variable.boolean varName
 
-    v.Data.toInt |> should equal 0
-    v.Data.toFloat |> should equal 0.0
-    v.State |> should be False
+    v.Value.toInt |> should equal 0
+    v.Value.toFloat |> should equal 0.0
     v.Name |> should equal varName
+    Variable.state(v) |> should be False
 
   [<Fact>]
   let ``create number variable with default values``() =
@@ -24,83 +24,83 @@ module ``Basic Types`` =
     let lb = -1.0
     let ub = 2.0
 
-    let v = Variable.Real(varName, lb, ub)
+    let v = Variable.real varName lb ub
 
-    v.LowerBound.toFloat |> should equal (lb)
-    v.UpperBound.toFloat |> should equal (ub)
+    v.Bounds.Lower.toFloat |> should equal lb
+    v.Bounds.Upper.toFloat |> should equal ub
 
-    v.Data.toFloat |> should equal (0.0)
+    v.Value.toFloat |> should equal Double.NegativeInfinity
     v.Name |> should equal varName
 
   [<Fact>]
   let ``create boolean operand`` () =
-    let x = Variable.Bool "a"
+    let x = Variable.boolean "a" |> toExpression
     let result = 1*x
     result |> should be instanceOfType<Expression>
 
   [<Fact>]
   let ``create number (integer) operand`` () =
-    let x = Variable.Integer("a", 0, 1)
+    let x = Variable.integer "a" 0 1 |> toExpression
     let result = 1*x
     result |> should be instanceOfType<Expression>
-    result.Statement |> should haveLength 1
+    result |> should haveLength 1
 
 
   [<Fact>]
-  let ``create number (float) operand`` () =
-    let x = Variable.Real("a", 0., 1.)
+  let ``create number--float--operand`` () =
+    let x = Variable.real "a" 0. 1. |> toExpression
     let result = 1*x
     result |> should be instanceOfType<Expression>
-    result.Statement |> should haveLength 1
+    result |> should haveLength 1
 
 
   [<Fact>]
-  let ``add two (integer) variables and create an expression``() =
-    let x = Variable.Integer "a"
-    let y = Variable.Integer "b"
+  let ``add two--integer--variables and create an expression``() =
+    let x = Variable.integer "a" Int32.MinValue Int32.MaxValue |> toExpression
+    let y = Variable.integerDefault "b" |> toExpression
 
     let result = x + y
     result |> should be instanceOfType<Expression>
-    result.Statement |> should haveLength 2
+    result |> should haveLength 2
 
 
   [<Fact>]
-  let ``add number variable (integer) and constant should create an expression``() =
-    let x = Variable.Integer "a"
+  let ``add number variable--integer--and constant should create an expression``() =
+    let x = Variable.integerDefault "a" |> toExpression
 
     let result = x + 77
     result |> should be instanceOfType<Expression>
-    result.Statement |> should haveLength 2
+    result |> should haveLength 2
 
 
   [<Fact>]
-  let ``add number variable (real) and constant should create an expression``() =
-    let x = Variable.Real "a"
+  let ``add number variable--real--and constant should create an expression``() =
+    let x = Variable.realDefault "a" |> toExpression
 
     let result = x + 77.0
     result |> should be instanceOfType<Expression>
-    result.Statement |> should haveLength 2
+    result |> should haveLength 2
 
 
   [<Fact>]
   let ``add number variable (real) and coefficient-variable (real) should create an expression``() =
-    let x = Variable.Real "a"
-    let y = Variable.Real "b"
+    let x = Variable.realDefault "a" |> toExpression
+    let y = Variable.realDefault "b" |> toExpression
 
     let result = x + 2*y
     result |> should be instanceOfType<Expression>
-    result.Statement |> should haveLength 2
+    result |> should haveLength 2
 
 
   [<Fact>]
   let ``create a multi-variable expression from integer variables`` () =
-    let x = Variable.Integer("a", 0, 10)
-    let y = Variable.Integer("b", 0, 10)
-    let z = Variable.Integer("c", 0, 10)
+    let x = Variable.integer "a" 0 10 |> toExpression
+    let y = Variable.integer "b" 0 10 |> toExpression
+    let z = Variable.integer "c" 0 10 |> toExpression
 
     let result = x + 2*y + 5*z + 80
     result |> should be instanceOfType<Expression>
-    result.Statement |> should haveLength 4
+    result |> should haveLength 4
 
 
   [<Fact>]
@@ -108,12 +108,12 @@ module ``Basic Types`` =
     let varName = "real"
     let lb = -1.0
     let ub = 2.0
-    let mutable v = Variable.Real(varName, lb, ub)
+    let mutable v = Variable.real varName lb ub
 
     let newValue = 1.0
-    v.Data.toFloat |> should equal 0.0
-    v <- v.Set(newValue)
-    v.Data.toFloat |> should equal newValue
+    v.Value.toFloat |> should equal Double.NegativeInfinity
+    v <- v |> Variable.set newValue
+    v.Value.toFloat |> should equal newValue
 
 
   [<Fact>]
@@ -121,36 +121,35 @@ module ``Basic Types`` =
     let varName = "real"
     let lb = -1.0
     let ub = 2.0
-    let v = Variable.Real(varName, lb, ub)
+    let v = Variable.real varName lb ub
 
     let outOfBoundsValue = 9.0
-    shouldFail (fun () -> v.Set outOfBoundsValue |> should throw typeof<System.ArgumentOutOfRangeException> )
+    shouldFail (fun () -> v |> Variable.set outOfBoundsValue |> should throw typeof<System.ArgumentOutOfRangeException> )
 
   [<Fact>]
   let ``set boolean variable throws error if set to number`` () =
-    let v = Variable.Bool "bool"
+    let v = Variable.boolean "bool"
 
     let invalidValue = 9.0
-    shouldFail (fun () -> v.Set invalidValue |> should throw typeof<System.ArgumentOutOfRangeException> )
+    shouldFail (fun () -> v |> Variable.set invalidValue |> should throw typeof<System.ArgumentOutOfRangeException> )
 
   [<Fact>]
   let ``set number variable throws error if set to boolean`` () =
-    let v = Variable.Integer("num", 0, 1)
+    let v = Variable.integer "num" 0 1
 
     let invalidValue = true
-    shouldFail (fun () -> v.Set invalidValue |> should throw typeof<System.ArgumentOutOfRangeException> )
+    shouldFail (fun () -> v |> Variable.set invalidValue |> should throw typeof<System.ArgumentOutOfRangeException> )
 
   [<Fact>]
   let ``evaluate an expression with with all operand types``() =
-    let x = Variable.Integer "a"
-    let y = Variable.Integer "b"
 
     let value = 5
-    let domain = Set.ofList [x.Set(value); y.Set(value)]
+    let x = Variable.integerDefault "a" |> Variable.set value |> toExpression
+    let y = Variable.integerDefault "b" |> Variable.set value |> toExpression
+
     let expression = x + 2*y + 10
+    let result = eval(expression)
 
-    let coDomain = expression |> Expression.Eval(domain)
-
-    coDomain |> should be instanceOfType<Number>
-    coDomain.toInt |> should equal 25
+    result |> should be instanceOfType<Number>
+    result.toInt |> should equal 25
 
--- or/types.fs
+++ or/types.fs
@@ -35,176 +35,117 @@ module Types =
   end
 
 
-  type VariableData = {
-    Name:string
-    Bounds: NumberBounds
-    Value: Number
-   }
 
-  type Variable =
-    | Boolean of VariableData
-    | Number of VariableData
-    static member Bool (name:string) =
-      let bnd = {Lower=Number.Integer(0); Upper=Number.Integer(1); Interval=Interval.Include}
-      Boolean({Name=name; Bounds=bnd; Value=Number.Integer(0)})
-    static member Integer (name:string) =
-      let bnd = {Lower=Number.Integer(Int32.MinValue); Upper=Number.Integer(Int32.MaxValue); Interval=Interval.Include}
-      Number({Name=name; Bounds=bnd; Value=Number.Integer(0)})
-    static member Integer ((name:string), (lowerBound:int), (upperBound:int)) =
-      let bnd = {Lower=Number.Integer(lowerBound); Upper=Number.Integer(upperBound); Interval=Interval.Include}
-      Number({Name=name; Bounds=bnd; Value=Number.Integer(0)})
-    static member Real (name:string) =
-      let bnd = {Lower=Number.Real(Double.NegativeInfinity); Upper=Number.Real(Double.PositiveInfinity); Interval=Interval.Include}
-      Number({Name=name; Bounds=bnd; Value=Number.Real(0.0)})
-    static member Real ((name:string), (lowerBound:float), (upperBound:float)) =
-      let bnd = {Lower=Number.Real(lowerBound); Upper=Number.Real(upperBound); Interval=Interval.Include}
-      Number({Name=name; Bounds=bnd; Value=Number.Integer(0)})
-    member this.Name =
-      match this with
-      | Number({Name=n; Bounds=_; Value=_}) -> n
-      | Boolean({Name=n; Bounds=_; Value=_}) -> n
-    member this.Data =
-      match this with
-      | Number({Name=_; Bounds=_; Value=v}) -> v
-      | Boolean({Name=_; Bounds=_; Value=v}) -> v
-    member this.State =
-      match this with
-      | Boolean({Name=_; Bounds=_; Value=v}) ->
-          match v with
-          | Integer(i) -> i.Equals(1)
-          | Real(r) -> r.Equals(1.0)
-      | Number(_) -> failwith "Only can be used for Boolean variables."
-    member this.UpperBound =
-      match this with
-      | Number({Name=_; Bounds=bnd; Value=_}) -> bnd.Upper
-      | Boolean({Name=_; Bounds=bnd; Value=_}) -> bnd.Upper
-    member this.LowerBound =
-      match this with
-      | Number({Name=_; Bounds=bnd; Value=_}) -> bnd.Lower
-      | Boolean({Name=_; Bounds=bnd; Value=_}) -> bnd.Lower
-    member this.Set (s:obj) =
-      match this, s with
-      | Boolean({Name=n; Bounds=b; Value=Number.Integer(_)}), (:? bool as bl) ->
-          match bl with
-          | true -> Number({Name=n; Bounds=b;Value=Number.Integer(1)})
-          | false -> Number({Name=n; Bounds=b;Value=Number.Integer(0)})
-      | Boolean({Name=n; Bounds=b; Value=Number.Integer(_)}), (:? int as i) ->
-          match i with
-          | 1 -> Number({Name=n; Bounds=b;Value=Number.Integer(1)})
-          | 0 -> Number({Name=n; Bounds=b;Value=Number.Integer(0)})
-          | _ -> failwith "Only 1 or 0 are acceptable values"
-      | Number({Name=n; Bounds=b; Value=Number.Integer(_)}), (:? int as i) ->
-          match b.withinBounds(i) with
-          | true ->  Number({Name=n; Bounds=b;Value=Number.Integer(i)})
-          | false -> invalidArg "s" "Out of Range"
-      | Number({Name=n; Bounds=b; Value=Number.Real(_)}), (:? float as f) ->
-          match b.withinBounds(f) with
-          | true ->  Number({Name=n; Bounds=b;Value=Number.Real(f)})
-          | false -> invalidArg "s" "Out of Range"
-      | Number({Name=n; Bounds=b; Value=Number.Real(_)}), (:? int as i) ->
-          match b.withinBounds(i) with
-          | true ->  Number({Name=n; Bounds=b;Value=Number.Real(float(i))})
-          | false -> invalidArg "s" "Out of Range"
-      | Number({Name=n; Bounds=b; Value=Number.Integer(_)}), (:? float as f) ->
-          match b.withinBounds(f) with
-          | true ->  Number({Name=n; Bounds=b;Value=Number.Integer(int(f))})
-          | false -> invalidArg "s" "Out of Range"
-      | _ -> failwith "Unable to set variable"
-    static member (*) (c:float, v:Variable): Expression =
-      let exp = Expression.New()
-      {
-        exp with
-          Statement = exp.Statement@[CoefficientArgument(Number.Real(c), v)];
-          Variables = exp.Variables.Add(v.Name, v)
-      }
-    static member (*) (c:int, v:Variable) : Expression =
-      let exp = Expression.New()
-      {
-        exp with
-          Statement = exp.Statement@[CoefficientArgument(Number.Integer(c), v)];
-          Variables = exp.Variables.Add(v.Name, v)
-      }
-    static member (+) ((arg1:Variable), (arg2:Variable)) : Expression =
-      let exp = Expression.New()
-      {
-        exp with
-          Statement = exp.Statement@[Argument(arg1); Argument(arg2)];
-          Variables = exp.Variables.Add(arg1.Name, arg1).Add(arg2.Name, arg2)
-      }
-    static member (+) ((arg1:Variable), (arg2:int)) : Expression =
-      let exp = Expression.New()
-      {
-        exp with
-          Statement = exp.Statement@[Argument(arg1); Constant(Number.Integer(arg2))];
-          Variables = exp.Variables.Add(arg1.Name, arg1).Add(arg2.ToString(), Variable.Integer(arg2.ToString(), arg2, arg2))
-      }
-    static member (+) ((arg1:Variable), (arg2:float)) : Expression =
-      let exp = Expression.New()
-      {
-        exp with
-          Statement = exp.Statement@[Argument(arg1); Constant(Number.Real(arg2))];
-          Variables = exp.Variables.Add(arg1.Name, arg1).Add(arg2.ToString(), Variable.Real(arg2.ToString(), arg2, arg2))
-      }
-    static member (+) ((arg1:Variable), (arg2:Expression)) : Expression =
-      {
-        arg2 with
-          Statement = [Argument(arg1)]@arg2.Statement;
-          Variables = arg2.Variables.Add(arg1.Name, arg1)
-      }
-    static member (+) ((arg1:Expression), (arg2:Variable)) : Expression =
-      {
-        arg1 with
-          Statement = arg1.Statement@[Argument(arg2)]
-          Variables = arg1.Variables.Add(arg2.Name, arg2)
-      }
-  and Operand =
-    | Constant of Number
-    | Argument of Variable
-    | CoefficientArgument of Number * Variable
-  and Expression = {
-    Statement: Operand list
-    Variables: Map<string, Variable>
+  /// Operand in expression
+  type Term = {
+    Name: string
+    Coefficient: Number
+    Bounds: NumberBounds
+    Exponent: Number
+    Value: Number
+    IsBoolean: bool
+  }
+
+
+  type Expression = {
+    Terms: Term list
   }
   with
-  static member New() = {Statement=List.empty; Variables=Map.empty}
-  static member Eval (vars:Set<Variable>) (exp:Expression): Number =
-    if vars.IsEmpty then invalidArg "vars" "Set cannot be empty"
+  static member (*) (a:obj, b:obj) =
+    match a, b with
+    | (:? int as s), (:? Expression as t) when t.Terms.Length = 1 -> { Terms=[{ t.Terms.[0] with Coefficient=Number.Integer(s) }] }
+    | (:? float as s), (:? Expression as t) when t.Terms.Length = 1 -> { Terms=[{ t.Terms.[0] with Coefficient=Number.Real(s) }] }
+    | (:? Expression as t), (:? int as s) when t.Terms.Length = 1 -> { Terms=[{ t.Terms.[0] with Coefficient=Number.Integer(s) }] }
+    | (:? Expression as t), (:? float as s) when t.Terms.Length = 1 -> { Terms=[{ t.Terms.[0] with Coefficient=Number.Real(s) }] }
+    | _ -> failwith "Cannot use (*) operator on expression"
+  static member (+) (a:obj, b:obj) =
+    match a, b with
+    | (:? int as s), (:? Expression as t) ->
+        {Terms=[{ Name = sprintf "%d" s; Coefficient = Number.Integer(1); Exponent = Number.Integer(1); Bounds = { Lower=Integer(s); Upper=Integer(s); Interval=Include }; Value = Number.Integer(s); IsBoolean=false}]@t.Terms }
+    | (:? Expression as t), (:? int as s) ->
+        {Terms=t.Terms@[{ Name = sprintf "%d" s; Coefficient = Number.Integer(1); Exponent = Number.Integer(1); Bounds = { Lower=Integer(s); Upper=Integer(s); Interval=Include }; Value = Number.Integer(s); IsBoolean=false}]}
+    | (:? float as s), (:? Expression as t) ->
+        {Terms=[{ Name = sprintf "%f" s; Coefficient = Number.Integer(1); Exponent = Number.Integer(1); Bounds = { Lower=Real(s); Upper=Real(s); Interval=Include }; Value = Number.Real(s); IsBoolean=false}]@t.Terms}
+    | (:? Expression as t), (:? float as s) ->
+        {Terms=t.Terms@[{ Name = sprintf "%f" s; Coefficient = Number.Integer(1); Exponent = Number.Integer(1); Bounds = { Lower=Real(s); Upper=Real(s); Interval=Include }; Value = Number.Real(s); IsBoolean=false}]}
+    | (:? Expression as s), (:? Expression as t) ->
+        {Terms=s.Terms@t.Terms}
+    | _ -> failwith "Cannot use (+) operator on expression"
 
-    let tmp = List.map (fun (v:Variable) ->  exp.Variables.ContainsKey(v.Name) ) (vars |> Set.toList)
-    let allVariablesPresent = tmp |> List.reduce (&&)
-    if not allVariablesPresent then invalidArg "vars" "one (or many) input variable names do not match expression variable names"
-
-    let ans = (exp.Statement) |> List.map ( fun (v:Operand) ->
-      match v with
-      | Constant(c) -> c.toFloat
-      | Argument(a) ->
-          let ap = List.find (fun (f:Variable) -> f.Name.Equals(a.Name)) (vars |> Set.toList)
-          ap.Data.toFloat
-      | CoefficientArgument(c,a) ->
-          let ap = List.find (fun (f:Variable) -> f.Name.Equals(a.Name)) (vars |> Set.toList)
-          c.toFloat * ap.Data.toFloat
-    )
-
-    Number.Real(ans |> List.reduce (+))
-  static member (+) ((arg1:Expression), (arg2:Expression)) : Expression =
-    {
-      arg1 with
-        Statement = arg1.Statement@arg2.Statement
-        Variables = Map.fold (fun acc key value -> Map.add key value acc) arg1.Variables arg2.Variables
-    }
-  static member (+) ((arg1:Expression), (arg2:int)) : Expression =
-    {
-      arg1 with
-        Statement = arg1.Statement@[Constant(Number.Integer(arg2))]
-        Variables = arg1.Variables.Add(arg2.ToString(), Variable.Integer(arg2.ToString(), arg2, arg2))
-    }
-  static member (+) ((arg1:Expression), (arg2:float)) : Expression =
-    {
-      arg1 with
-        Statement = arg1.Statement@[Constant(Number.Real(arg2))]
-        Variables = arg1.Variables.Add(arg2.ToString(), Variable.Real(arg2.ToString(), arg2, arg2))
-    }
   end
+
+  /// Term in model that can change
+  module Variable =
+    /// Boolean Variable
+    /// Special Term with only acceptable values being {0, 1}
+    let boolean (name:string) : Term =
+      {
+        Name = name;
+        Coefficient = Number.Integer(1);
+        Exponent = Number.Integer(1);
+        Bounds = { Lower=Integer(0); Upper=Integer(1); Interval=Include };
+        Value = Number.Integer(0);
+        IsBoolean = true
+      }
+
+    /// Variable in the domain of real numbers
+    let real (name:string) (lowerBound:float) (upperBound:float) : Term =
+      {
+        Name = name;
+        Coefficient = Number.Real(1.0);
+        Exponent = Number.Real(1.0);
+        Bounds = { Lower=Real(lowerBound); Upper=Real(upperBound); Interval=Include };
+        Value = Number.Real(Double.NegativeInfinity);
+        IsBoolean = false
+      }
+
+    /// named variable with default bounds being the integer minimum and maximum of OS platform
+    let realDefault (name:string) : Term =
+      real name Double.PositiveInfinity Double.PositiveInfinity
+
+    /// Variable in the domain of integer numbers
+    let integer (name:string) (lowerBound:int) (upperBound:int) : Term =
+      {
+        Name = name;
+        Coefficient = Number.Real(1.0);
+        Exponent = Number.Real(1.0);
+        Bounds = { Lower=Integer(lowerBound); Upper=Integer(upperBound); Interval=Include };
+        Value = Number.Integer(Int32.MinValue);
+        IsBoolean = false
+      }
+
+    /// named variable with default bounds being the integer minimum and maximum of OS platform
+    let integerDefault (name:string) : Term =
+      integer name Int32.MinValue Int32.MaxValue
+
+    /// sets the value of the term
+    let set (value:obj) (t:Term) : Term =
+      match value with
+      | :? bool as b  when t.IsBoolean = true -> {t with Value = Number.Integer(1)}
+      | :? bool as b  when t.IsBoolean = false -> {t with Value = Number.Integer(0)}
+      | :? int as i  when t.IsBoolean = true -> invalidArg "value" "cannot set boolean variable with input"
+      | :? float as f  when t.IsBoolean = true -> invalidArg "value" "cannot set boolean variable with input"
+      | :? int as i -> {t with Value = Number.Integer(i)}
+      | :? float as f -> {t with Value = Number.Real(f)}
+      |_ -> invalidArg "t" "Out of Range"
+
+    /// The state of a boolean variable. All other values return an error.
+    let state (t:Term): bool =
+      match t.IsBoolean with
+      | true  when t.Value.toInt.Equals(1) -> true
+      | true  when t.Value.toInt.Equals(0) -> false
+      | _ -> failwith "state function only can be used for Boolean variables"
+
+
+
+  /// Converts term to expression to be used in equations
+  let toExpression (t:Term) : Expression = {Terms=[t]}
+
+  let eval (exp:Expression) : Number =
+    let a = List.map (fun term -> term.Coefficient.toFloat * Math.Pow(term.Value.toFloat, term.Exponent.toFloat)) exp.Terms
+    let result = List.reduce (+) a
+    Number.Real(result)
+
 
   type Constraint = Constraint of Expression * bounds:NumberBounds
 
